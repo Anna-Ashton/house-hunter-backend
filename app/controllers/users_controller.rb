59b2224e118f68a93d9ecf5d2c245a33060f0849
nziokikevin <kevinnzioki10@gmail.com>
2023-01-12 01:49:49 +0300

    adding action to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,34 @@
 class UsersController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+  wrap_parameters format: []
 
+  def index
+    users = User.all
+    render json: users
+  end
 
+  def show
+    user = User.find(params[:id])
+    render json: user
+  end
+
+  def create
+    user = User.create(user_params)
+    render json: user, status: :created
+  end
+
+  private
+
+  def user_params
+    params.permit(:full_name, :username, :email, :contact)
+  end
+
+  def render_not_found_response
+    render json: { error: "User not found" }, status: :not_found
+  end
+
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+  end
 end
