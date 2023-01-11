a99ff37914ba865308448edf1c6382c2648e6164
Anna-Ashton <ann.karanja@student.moringaschool.com>
2023-01-12 02:38:05 +0300

    login or authorizing the landlord

---
+++ app/controllers/auth_controller.rb
@@ -0,0 +1,14 @@
+class AuthController < ApplicationController
+    skip_before_action :authorized, only:[:create]
+
+    def create
+        @landlord= Landlord.find_by(username: params[:username])
+
+        if @landlord && @landlord.authenticate(params[:password])
+            token = encode_token({landlord_id: @landlord.id})
+            render json: { landlord: @landlord, token: token }, status: :accepted
+        else
+            render json: { errors: "Invalid email or password" }, status: :unauthorized
+        end
+    end
+end
