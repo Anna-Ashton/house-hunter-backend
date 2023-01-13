8f06f10bd13283272e43038dc5274dcc818cfa93
Anna-Ashton <ann.karanja@student.moringaschool.com>
2023-01-13 13:59:58 +0300

    skip before action in users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  skip_before_action :authorized, only: [:index, :show, :create]
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
   wrap_parameters format: []
