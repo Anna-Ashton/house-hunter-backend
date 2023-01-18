de0fb22635230ac4f3ca540b00a7ae601bdb9152
Anna-Ashton <ann.karanja@student.moringaschool.com>
2023-01-18 21:15:56 +0300

    Changed serializer

--- app/serializers/landlord_serializer.rb
+++ app/serializers/landlord_serializer.rb
@@ -1,3 +1,3 @@
 class LandlordSerializer < ActiveModel::Serializer
-  attributes :id, :full_name, :username, :email, :caretaker_name,:contacts, :password_digest
+  attributes :id, :full_name, :username, :email, :caretaker_name,:contacts
 end
